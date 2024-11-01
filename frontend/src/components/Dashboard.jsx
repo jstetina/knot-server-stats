9756407729d8f23ea0c967465f0d96f86280dd40
jstetina <jakub.stetina2003@gmail.com>
2024-11-01 20:49:03 +0100

    dashboard: Request server stat data

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,9 +2,33 @@ import { Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import ServerCard from "./ServerCard.jsx";
 import { server_stats } from "../placeholders/server_stats.js";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Dashboard() {
-  const data = server_stats;
+  const [data, setData] = useState(server_stats);
+  const getStats = async () => {
+    try {
+      const response = await axios.get("https://api.fit-vut.cz/get-stats", {
+        withCredentials: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
+
+      if (response.status === 200) {
+        setData(response);
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error(
+        "Login failed:",
+        error.response?.data?.message || error.message,
+      );
+      return false;
+    }
+  };
+  useEffect(getStats(), []);
+
   return (
     <Container style={{ marginTop: "20px" }}>
       <Typography variant="h4" gutterBottom>
